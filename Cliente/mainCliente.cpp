30dfd13610eb4153a1e1d0209305a02ba4dbe3c2
Thiago Azevedo Alves <taalves@inf.ufpel.edu.br>
2011-12-05 21:32:07 -0200

    Cliente e Servidor prontos com todas as funcionalidades, preparando relatório!

--- Cliente/mainCliente.cpp
+++ Cliente/mainCliente.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -9,42 +11,70 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 	
-	if (argc != 2){
-		cout << "Argumentos inválidos: ./cliente <ip>" << endl;
+	if (argc != 3){
+		cout << "Argumentos inválidos: ./cliente <ip do servidor> <chave privada>" << endl;
 		exit(1);
 	}
 	int sock, numbytes;
 	struct sockaddr_in serv_addr;
-	char buffer[255];
+	char buffer[4000];
+	char string[256];
+	ofstream arquivo;
+	
 	
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		//perror("socket");
 		cout << "Erro ao criar porta!"
 			 << endl;
 		exit(1);
 	}
 	
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(5555);
+	serv_addr.sin_port = htons(5678);
 	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-			 //perror("connect");
+	
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { //conecta com o servidor
 			 cout << "Erro ao conectar com o servidor!" 
 				  << endl;
 			 exit(1);
 	};
-	if ((numbytes=recv(sock, buffer,254, 0)) < 0) {
-		 //perror("recv");
-		 cout << "Erro ao receber arquivo do servidor!"
-			  << endl;
-		 exit(1);
-	}
+	
+	//agora recebe a hash do arquivo ------------------------------------------------------------------------------------------------------ //
+	
+	arquivo.open("hash.txt");
+	
+	numbytes=recv(sock, buffer,3999, 0);
 	buffer[numbytes] = '\0';
-	cout << "recebeu do servidor: "
-		 << buffer
-		 << endl;
-
+	arquivo << buffer;
+	
+	arquivo.close();
+	//até aqui é recebendo a hash e salvando no arquivo hash.txt --------------------------------------------------------------------------- //
+	
+	//agora recebe a hash criptografada com a chave privada do servidor -------------------------------------------------------------------- //
+	
+	arquivo.open("signed_digest_file");
+	
+	numbytes=recv(sock, buffer,3999, 0);
+	buffer[numbytes] = '\0';
+	arquivo << buffer;
+	arquivo.close();
+	
+	//agora recebe o arquivo criptografado com a chave publica do cliente e desencriptografa com a chave privada -----------------------------//
+	
+	arquivo.open("texto_cifrado.bin");
+	
+	numbytes=recv(sock, buffer,3999, 0);
+	buffer[numbytes] = '\0';
+	arquivo << buffer;
+	arquivo.close();
+	
 	close(sock);
 	
+	string[0] = '\0';
+	strcat(string, "openssl rsautl -in texto_cifrado.bin -out arquivo.txt  -inkey ");
+	strcat(string, argv[2]);
+	strcat(string, " -decrypt");
+	system(string);
+	
+	// ok!
 	return 0;
 }
--- Servidor/mainServ.cpp
+++ Servidor/mainServ.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <cstdlib>
-#include "funcsServ.h"
 
 using namespace std;
 
-int main(){
-	ifstream hash;
+int main(int argc, char *argv[]){
+	//*argv[0] == nome do programa;
+	//*argv[1] == arquivo contendo chave pública do cliente;
+	
+	if (argc != 2){
+		cout << "Parâmetros inválidos!"
+			 << "./servidor <chave publica do cliente>"
+			 << endl;
+		exit(1);
+	}
+	
+	
 	ifstream arquivo;
 	int sock, novo_sock, tamanho; //sockets;
 	struct sockaddr_in serv_addr, cl_addr; //estrutura para configuração do socket destino e origem;
+	char buffer[4000];
+	char string[256];
+	
+	
 	if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0){ //declara o novo socket testando se não houve erro;
 		cout << "ERRO DE SOCKET No " 
 			 << -sock //número do erro;
@@ -22,7 +38,7 @@ int main(){
 	}
 	serv_addr.sin_family = AF_INET; //protocolo internet arpa
 	serv_addr.sin_addr.s_addr = INADDR_ANY; //procura endereço disponivel de internet
-	serv_addr.sin_port = htons(5555); //número da porta
+	serv_addr.sin_port = htons(5678); //número da porta
 	
 	if (bind(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){ //realiza o bind testando erros;
 		perror("bind");
@@ -35,28 +51,66 @@ int main(){
 	cout << "Esperando conexões: "
 		 << endl;
 	
-	while(1){
-		tamanho = sizeof(struct sockaddr_in);
-		if ((novo_sock = accept(sock, (struct sockaddr*) &cl_addr, (socklen_t *)&tamanho)) < 0){
-			cout << "ERRO AO REALIZAR ACCEPT()" 
-				 << endl;
-			exit(1);
-		}
-		cout << "Servidor recebeu pedido de conexão de: "
-			 << inet_ntoa(cl_addr.sin_addr)
+	tamanho = sizeof(struct sockaddr_in);
+	if ((novo_sock = accept(sock, (struct sockaddr*) &cl_addr, (socklen_t *)&tamanho)) < 0){
+		cout << "ERRO AO REALIZAR ACCEPT()" 
 			 << endl;
-		if (fork()==0){ // se for filho 
-			if (send(novo_sock, "Hello, world!\n", 14, 0) == -1){
-				perror("send");
-				cout << "Erro ao enviar mensagem!"
-					 << endl;
-				close(novo_sock);
-				exit(0);			
-			}
+		exit(1);
+	}
+	cout << "Servidor recebeu pedido de conexão de: "
+		 << inet_ntoa(cl_addr.sin_addr)
+		 << endl;
+	if (fork()==0){ // se for filho 
+	
+	//agora envia um arquivo hash.txt do arquivo que vai ser enviado para garantir a integridade ---------------------------------------//
+		
+		system("openssl dgst -md5 arquivo.txt > hash.txt");
+		
+		arquivo.open("hash.txt");
+		arquivo.getline(buffer, 3999);
+		if (send(novo_sock, buffer, (int)strlen(buffer), 0) == -1){
+			perror("send");
+			cout << "Erro ao enviar hash!"
+				 << endl;
 			close(novo_sock);
+			exit(0);			
 		}
- 
+		arquivo.close();
+		
+		//criptografa a hash com a chave privada do servidor e reenvia, para garantir a autenticidade --------------------------------------//
+		
+		system("openssl dgst -md5 -sign chave_privada_servidor.key -out signed_digest_file hash.txt");
+		
+		arquivo.open("signed_digest_file");
+		arquivo.getline(buffer, 3999);
+		if (send(novo_sock, buffer, (int)strlen(buffer), 0) < 0){
+			perror("send");
+			cout << "Erro ao enviar digest!"
+				 << endl;
+			close(novo_sock);
+			exit(0);		
+		}
+		arquivo.close();
+		
+		//agora envia o arquivo criptografado com a chave publica do cliente;
+		string[0] = '\0';
+		strcat(string, "openssl rsautl -in arquivo.txt -out texto_cifrado.bin -pubin -inkey ");
+		strcat(string, argv[1]);
+		strcat(string, " -encrypt");
+		system(string);
+		
+		arquivo.open("texto_cifrado.bin");
+		arquivo.getline(buffer, 3999);
+		if (send(novo_sock, buffer, (int)strlen(buffer), 0) < 0){
+			perror("send");
+			cout << "Erro ao enviar digest!"
+				 << endl;
+			close(novo_sock);
+			exit(0);		
+		}
+		arquivo.close();
+		
+		close(novo_sock);
 	}
 	return 0;
-	
 }
